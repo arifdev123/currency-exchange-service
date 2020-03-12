977ae7933fe3cd956811a2f6a0bb8c30b69dcb37
Mohamed Ismail Arif <mhdarif.vdm@gmail.com>
2020-03-12 13:02:41 +0530

    upto zuul api gateway implementation done

---
+++ src/test/java/co/in/javacoder/microservices/currencyexchangeservice/CurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.in.javacoder.microservices.currencyexchangeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
